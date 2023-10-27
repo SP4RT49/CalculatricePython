86da4257ff392058349e68ce10ac54133a293459
zeqotibo <thibaultderouin5@gmail.com>
2023-10-27 11:01:39 +0200

    feat: add calcul with ²

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import re
+
 def addition(x, y):
     return x + y
 
@@ -19,15 +21,28 @@ def main():
         print("Entrez 'sub' ou '-' pour soustraire deux nombres")
         print("Entrez 'mul' ou '*' pour multiplier deux nombres")
         print("Entrez 'div' ou '/' pour diviser deux nombres")
-        
+
         user_input = input("> ")
 
         if user_input not in ('add', 'sub', 'mul', 'div', '+', '-', '*', '/'):
             print("Option non valide!")
             continue
 
-        num1 = float(input("Entrez le premier nombre: "))
-        num2 = float(input("Entrez le deuxième nombre: "))
+        num1 = input("Entrez le premier nombre: ")
+        puissance_match = re.match(r'(\d+)²', num1)
+        if puissance_match:
+            base = float(puissance_match.group(1))
+            num1 = base ** 2
+        else:
+            num1 = float(num1)
+
+        num2 = input("Entrez le deuxième nombre: ")
+        puissance_match = re.match(r'(\d+)²', num2)
+        if puissance_match:
+            base = float(puissance_match.group(1))
+            num2 = base ** 2
+        else:
+            num2 = float(num2)
 
         if user_input == "add" or user_input == "+":
             print("Le résultat de l'addtion est", addition(num1, num2))
